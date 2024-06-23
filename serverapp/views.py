1c709dd8e43800a6cc7071c112d7c3b37ac4fce3
Jean-Lionel <nijeanlionel@gmailcom>
2024-06-23 11:58:15 +0200

    get propretiers of a computer

--- serverapp/views.py
+++ serverapp/views.py
@@ -1,4 +1,7 @@
 from django.contrib.auth.models import Group, User
+from django.http import HttpResponse
+
+import psutil
 from rest_framework import permissions, viewsets
 
 from tutorial.quickstart.serializers import GroupSerializer, UserSerializer
@@ -20,3 +23,30 @@ class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all().order_by('name')
     serializer_class = GroupSerializer
     permission_classes = [permissions.IsAuthenticated]
+    
+def getServerPropreties(request):
+    cpus = psutil.cpu_percent(percpu=True)
+    data = {
+			"disk":{
+				"total": round(psutil.disk_usage("/").total/2**30, 2),
+				"used": round(psutil.disk_usage("/").used/2**30, 2)
+			},
+			"ram":{
+				"total": round(psutil.virtual_memory().total/2**30, 2),
+				"used": round(psutil.virtual_memory().used/2**30, 2)
+			},
+			"swap":{
+				"total": round(psutil.swap_memory().total/2**30, 2),
+				"used": round(psutil.swap_memory().used/2**30, 2)
+			},
+		}
+    
+    
+    for i, cpu in enumerate(cpus):
+        data[f"cpu_{i}"] = {
+				"total": 100,
+				"used": cpu
+			}
+    print(data)
+		
+    return HttpResponse(data, 200)
