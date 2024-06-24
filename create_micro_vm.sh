4e27ee25dafb09931e28eb17642b17c68b50f14a
Jean-Lionel <nijeanlionel@gmailcom>
2024-06-24 06:28:42 +0200

    accept access

--- create_micro_vm.sh
+++ create_micro_vm.sh
@@ -99,8 +99,10 @@ sudo curl -X PUT --unix-socket "${API_SOCKET}" \
 # started before we attempt to SSH into it.
 sleep 2s
 
+chmod 700 ubuntu-22.04.id_rsa
+
 # Setup internet access in the guest
-ssh -i ./ubuntu-22.04.id_rsa root@$7 "ip route add default via ${TAP_IP} dev eth0" -y
+ssh -i ./ubuntu-22.04.id_rsa root@$7 "ip route add default via ${TAP_IP} dev eth0"
 
 # Setup DNS resolution in the guestip
-ssh -i ./ubuntu-22.04.id_rsa root@$7 "echo 'nameserver 8.8.8.8' > /etc/resolv.conf" -y
+ssh -i ./ubuntu-22.04.id_rsa root@$7 "echo 'nameserver 8.8.8.8' > /etc/resolv.conf"
